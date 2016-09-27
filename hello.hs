46d9efbf69f67698acfb78614e5c801526a73090
Joseph <jjinking@gmail.com>
2016-09-27 19:35:04 -0400

    Finished exercises for First Steps

--- hello.hs
+++ hello.hs
@@ -1,7 +1,22 @@
 module Main where
 import System.Environment
 
+-- main :: IO ()
+-- main = do
+--   args <- getArgs
+--   arg1 <- read (args !! 0)
+--   arg2 <- read (args !! 1)
+--   result <- show (arg1 + arg2)
+--   putStrLn ("Sum: " ++ result)
+
+
+-- main :: IO ()
+-- main = do
+--   args <- getArgs
+--   putStrLn (show (read (args !! 0) + read (args !! 1)))
+
+
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Arg1: " ++ args !! 0 ++ ", Arg2: " ++ args !! 1)
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
